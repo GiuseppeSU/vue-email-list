86910d8a972b7a50e8bd1ea52cbf560e848ac8c1
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-03-23 14:58:31 +0100

    fine esercizio

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      stampaEmail:[],
+      
+    }
+  
+  },
+  methods: {
+        
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+                this.stampaEmail.push(response.data.response);
+            }
+        );
+
+    }
+    
+
+ 
+
+  }
+}).mount('#app')
